14e295419ce69519346c9366c1fd6c1939f378d2
kyazuki <20392096+kyazuki@users.noreply.github.com>
2024-02-12 13:13:56 +0900

    servers.datバイナリ生成用スクリプトを削除
    ・別リポジトリに移動のため。

--- client/serversDat.ps1
+++
@@ -1,6 +0,0 @@
-# servers.datをバイナリ出力するスクリプト
-Param([parameter(mandatory = $true)][String]$ServersDatPath)
-Write-Output Windows:
-(Format-Hex $ServersDatPath).HexBytes | Join-String -Separator ' '
-Write-Output Mac:
-(Format-Hex $ServersDatPath).HexBytes | ForEach-Object { $_ -replace "^| ", "\x" } | Join-String
