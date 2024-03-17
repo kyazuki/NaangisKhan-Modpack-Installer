db95f81d82bf45987d890e1ed7172d08876308e0
kyazuki <20392096+kyazuki@users.noreply.github.com>
2024-03-17 19:06:09 +0900

    タイトル画面に表示されるqボタンのポップアップを抑制

--- client/update.bat
+++ client/update.bat
@@ -73,6 +73,7 @@ REM "v1.1.0アップデート"
 call :is_old_version 1.1.0
 if not errorlevel 1 (
     echo v1.1.0アップデートの適用を開始します...
+    REM "Maturi Delightを削除"
     del /q "%CD%\mods\maturidelight-1.20.1-3.0.0.jar" > nul 2>&1
     echo Modをアップデート中...
     del /q "%CD%\mods\supplementaries-1.20-2.7.35.jar" > nul 2>&1
@@ -254,6 +255,10 @@ if not errorlevel 1 (
         move /y config\cumulus_menus-client.toml.tmp config\cumulus_menus-client.toml > nul 2>&1
     ) else (
         echo タイトルメニューAPIの設定ファイルが見つかりません。 1>&2
+    )
+    REM "Quarkのタイトル画面に表示されるqボタンのポップアップを抑制"
+    if not exist .qmenu_opened.marker (
+        type nul > .qmenu_opened.marker
     )
 )
 exit /b 0
