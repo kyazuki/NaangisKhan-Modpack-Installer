eb81273e10e64ccd1ecf80ed59a891f58ae538b1
kyazuki <20392096+kyazuki@users.noreply.github.com>
2024-04-28 15:21:06 +0900

    Mac/Linuxで各種ファイルのダウンロードができない不具合を修正

--- client/run.sh
+++ client/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Javaがなければインストール
-java --version > nul 2>&1
+java --version > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     if [ "$(uname)" == 'Darwin']; then  # macOS
         echo -e Javaのインストールが必要です。下記リンクからインストールした後に再実行してください。\\nhttps://www.oracle.com/jp/java/technologies/downloads/#jdk17-mac 1>&2
         exit 1
     else    # Linux
-        sudo apt update > nul 2>&1
-        sudo apt install -y openjdk-17-jdk > nul 2>&1
+        sudo apt update > /dev/null 2>&1
+        sudo apt install -y openjdk-17-jdk > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             echo Javaのインストールに失敗しました。 1>&2
             exit 1
@@ -16,16 +16,16 @@ if [ $? -ne 0 ]; then
 fi
 # 設定ファイル・リソースをダウンロードして展開
 echo リソースをダウンロード中...
-curl -OsSf https://github.com/kyazuki/NaangisKhan-Modpack-Installer/releases/download/v1.1.5/client.zip 2>/dev/null
+curl -OLsSf https://github.com/kyazuki/NaangisKhan-Modpack-Installer/releases/download/v1.1.5/client.zip 2>/dev/null
 if [ $? -ne 0 ]; then
     echo リソースのダウンロードに失敗しました。 1>&2
     exit 1
 fi
-jar --version > nul 2>&1
+jar --version > /dev/null 2>&1
 if [ $? -eq 0 ]; then   # jarコマンドが使える場合
-    jar xf client.zip > nul 2>&1
+    jar xf client.zip > /dev/null 2>&1
 else # jarコマンドが使えない場合
-    unzip client.zip > nul 2>&1
+    unzip client.zip > /dev/null 2>&1
 fi
 if [ $? -ne 0 ]; then
     echo リソースの展開に失敗しました。 1>&2
@@ -34,7 +34,7 @@ fi
 rm client.zip
 # インストーラーをダウンロードして実行
 echo インストーラーをダウンロード中...
-curl -o minecraft-modpack-installer.jar -sSf https://github.com/kyazuki/Minecraft-Modpack-Installer/releases/download/v2.0.0/minecraft-modpack-installer-2.0.0.jar 2>/dev/null
+curl -o minecraft-modpack-installer.jar -LsSf https://github.com/kyazuki/Minecraft-Modpack-Installer/releases/download/v2.0.0/minecraft-modpack-installer-2.0.0.jar 2>/dev/null
 if [ $? -ne 0 ]; then
     echo インストーラーのダウンロードに失敗しました。 1>&2
 fi
